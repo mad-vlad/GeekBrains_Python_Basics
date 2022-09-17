f600de0661b9e959fbd0e3b7c046fcfe01a7260c
mad-vlad <vavanlo@gmail.com>
2022-09-17 15:17:06 +0200

    Sending 1st HW

--- Task 3.py
+++
@@ -1,6 +0,0 @@
-n = int(input("Please provide a number: "))
-decs = n * 10
-hundr = n * 100
-sum = n + (decs + n) + (hundr + decs + n)
-
-print(f"Provided number is {n} and in calculation {n}+{decs+n}+{hundr+decs+n} it equels: {sum}")
